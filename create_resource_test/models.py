dbe42fbf96208f369cce9fd5ffc12980e9025cf2
Ivan Toulou <touloumbadjiani@gmail.com>
2019-09-18 15:26:53 +0100

    Changed tech type models to Foreign keys in resource model and migrated

--- create_resource_test/models.py
+++ create_resource_test/models.py
@@ -4,20 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class ResourceTest(models.Model):
 
-    name = models.CharField(max_length=400)
-    link = models.CharField(max_length=1000, blank=True, null=True)
-    attachment = models.FileField(upload_to='documents', blank=True, null=True)
-    language = models.CharField(max_length=100, blank=True, null=True)
-    framework = models.CharField(max_length=100, blank=True, null=True)
-    database = models.CharField(max_length=100, blank=True, null=True)
-    technology = models.CharField(max_length=100, blank=True, null=True)
-    created_date = models.DateTimeField(auto_now_add=True, null=True)
-    # published_date = models.DateTimeField(blank=True, null=True, default=timezone.now())
-
-    def __str__(self):
-        return self.name
 
 class Language(models.Model):
     language = models.CharField(max_length=100)
@@ -42,3 +29,18 @@ class Technology(models.Model):
 
     def __str__(self):
         return self.technology
+
+class ResourceTest(models.Model):
+    name = models.CharField(max_length=400)
+    link = models.CharField(max_length=1000, blank=True, null=True)
+    attachment = models.FileField(upload_to='documents', blank=True, null=True)
+    language = models.ForeignKey(Language, default=None)
+    framework = models.ForeignKey(Framework, default=None)
+    database = models.ForeignKey(Database, default=None)
+    technology = models.ForeignKey(Technology, default=None)
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+
+    # published_date = models.DateTimeField(blank=True, null=True, default=timezone.now())
+
+    def __str__(self):
+        return self.name
